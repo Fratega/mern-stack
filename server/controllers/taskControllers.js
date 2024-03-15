3d6208828b2e1090d45b662e98359f2d3025a63f
Francisco Ortega <ortegareal1999gmail.com>
2024-03-15 19:11:24 -0300

    metodo put, delete añadido y envueltos los metodos en try/catch, listo nuestro servidor

--- server/controllers/taskControllers.js
+++ server/controllers/taskControllers.js
@@ -11,33 +11,65 @@ const connectionString = process.env.DATABASE_URL ?? config;
 const connection = await mysql.createConnection(connectionString);
 
 export const getTasks = async (req, res) => {
-  const [result] = await connection.query(
-    "SELECT * FROM tasks ORDER BY createdAt ASC"
-  );
-  res.json(result);
+  try {
+    const [result] = await connection.query(
+      "SELECT * FROM tasks ORDER BY createdAt ASC"
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const getTask = async (req, res) => {
-  const [result] = await connection.query(
-    "SELECT * FROM tasks WHERE id = ?",
-    [req.params.id]
-  );
-  res.json(result[0]);
+  try {
+    const [result] = await connection.query(
+      "SELECT * FROM tasks WHERE id = ?",
+      [req.params.id]
+    );
+
+    if (result.length === 0)
+      return res.status(404).json({ message: "Task not found" });
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const createTask = async (req, res) => {
-  const { title, description } = req.body;
-  const [result] = await connection.query(
-    "INSERT INTO tasks(title, description) VALUES (?, ?)",
-    [title, description]
-  );
-  res.json({ id: result.insertId, title, description });
+  try {
+    const { title, description } = req.body;
+    const [result] = await connection.query(
+      "INSERT INTO tasks(title, description) VALUES (?, ?)",
+      [title, description]
+    );
+    res.json({ id: result.insertId, title, description });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-export const updateTask = (req, res) => {
-  res.send("Editando tarea");
+export const updateTask = async (req, res) => {
+  try {
+    const result = await connection.query("UPDATE tasks SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+    res.json(result);
+  } catch (error) {}
 };
 
-export const deleteTask = (req, res) => {
-  res.send("Eliminando tarea");
+export const deleteTask = async (req, res) => {
+  try {
+    const [result] = await connection.query("DELETE FROM tasks WHERE id = ?", [
+      req.params.id,
+    ]);
+
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "Task not found" });
+
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
